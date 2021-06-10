663de7e8b56fd963977046788fc440b33c272c1a
michuW93 <michal.walkowiak93@gmail.com>
2021-06-10 11:08:07 +0200

    Function<Input, Output> .andThen

--- src/FunctionalProgramming/_Function.java
+++ src/FunctionalProgramming/_Function.java
@@ -9,7 +9,15 @@ public class _Function {
 
         int incrementByFunctionalProgramming = incrementByTwoFunction.apply(1);
         System.out.println(incrementByFunctionalProgramming);
+
+        Integer combinedResult = incrementByTwoFunction.andThen(multiplyBy20).apply(2);
+        System.out.println(combinedResult);
+
+        Function<Integer, Integer> incrementBy2AndMultiplyBy20 = incrementByTwoFunction.andThen(multiplyBy20);
+        System.out.println(incrementBy2AndMultiplyBy20.apply(2));
     }
+
+    static Function<Integer, Integer> multiplyBy20 = number -> number * 20;
 
     static Function<Integer, Integer> incrementByTwoFunction = number -> number + 2;
 
